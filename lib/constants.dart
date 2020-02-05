93843e88c7c43784c973db3eddd857b14e07aa31
Solomon APPIER-SIGN <braasig@gmail.com>
2020-02-05 06:48:34 +0000

    separated all constants

---
+++ lib/constants.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+const kBottomContainerHeight = 80.0;
+const kActiveCardColor = Color(0xff1D1E33);
+const kInactiveCardColor = Color(0xff111328);
+const kReusableCardColor = Color(0xff101427);
+const kLabelTextStyle = TextStyle(
+  fontSize: 18.0,
+  color: Color(0xff8D8E98),
+);
+const kNumberTextStyle = TextStyle(
+  fontSize: 50.0,
+  fontWeight: FontWeight.w900,
+);
