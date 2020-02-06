0a18b0befd907eb48734d43f7438265abad1eda8
Solomon APPIER-SIGN <braasig@gmail.com>
2020-02-06 09:08:15 +0000

    added button color constant

--- lib/constants.dart
+++ lib/constants.dart
@@ -4,6 +4,7 @@ const kBottomContainerHeight = 80.0;
 const kActiveCardColor = Color(0xff1D1E33);
 const kInactiveCardColor = Color(0xff111328);
 const kReusableCardColor = Color(0xff101427);
+const kGrayButton = Color(0xFF4C4F5E);
 const kLabelTextStyle = TextStyle(
   fontSize: 18.0,
   color: Color(0xff8D8E98),
